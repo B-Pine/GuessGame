34ddfe404047f24de798e367c70182e29ef0a661
Pine <bugingopine004@gmail.com>
2023-11-08 16:56:43 +0200

    Guess: Adding previous button Version 2

--- foreach.js
+++ foreach.js
@@ -9,18 +9,48 @@
 
 // console.log("Sum of element is: ",add());
 
+const questionArr = ['What is capital city of Rwanda?', 'In which continent does China belongs?', 'Who is the president of Rwanda?'];
 
-const answerParag = document.querySelector('.js-answer')
+const answerArr = ['Kigali', 'Asia', 'Kagame Paul'];
+
+const answerParag = document.querySelector('.js-answer');
+const qnParag = document.querySelector('.js-question');
+const showButton = document.querySelector('.js-show-answer')
 
 answerParag.style.display = "none";
+qnAnswerIndex = 0;
 
 function showAnswer () {
-    document.querySelector('.js-show-answer').addEventListener('click', () => {
-        answerParag.style.display = "block";
+    showButton.addEventListener('click', () => {
+        answerParag.innerHTML = `${answerArr[qnAnswerIndex]}`;
+
+        if (showButton.innerHTML === 'Show Answer') {
+            answerParag.style.display = "block";
+            showButton.innerHTML = 'Hide Answer';
+        }
+        else {
+            answerParag.style.display = "none";
+            showButton.innerHTML = 'Show Answer';
+        }
     })
 }
 
+document.querySelector('.js-next-qn').addEventListener('click', () => {
+    qnAnswerIndex++;
+    qnParag.innerHTML = `${questionArr[qnAnswerIndex]}`;
+    answerParag.style.display = "none";
+    showButton.innerHTML = 'Show Answer';
+});
+
+document.querySelector('.js-prev-qn').addEventListener('click', () => {
+    qnAnswerIndex--;
+    qnParag.innerHTML = `${questionArr[qnAnswerIndex]}`;
+    answerParag.style.display = "none";
+    showButton.innerHTML = 'Show Answer';
+});
 showAnswer();
+
+
 
 
 
